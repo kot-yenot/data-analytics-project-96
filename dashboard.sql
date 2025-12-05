cb6c67639e22ebfb01bf05c00626c8020b76f7b1
kot-yenot <lersiy@gmail.com>
2025-12-05 10:42:23 +0500

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,22 +1,27 @@
-SELECT 
+select
   visit_date,
   sum(leads_count),
   SUM(leads_count) OVER () AS total_leads,
   ROUND(SUM(leads_count) OVER (ORDER BY visit_date) * 100.0 / SUM(leads_count) OVER (), 2) AS percentage
-FROM voronka
-group by visit_date
-ORDER BY visit_date
+  from voronka
+  group by visit_date
+  order by visit_date
 
-SELECT sum(visitors_count), utm_source,  sum(total_cost), sum(revenue)
-from voronka 
-where utm_source = 'vk' or utm_source = 'yandex'
-group by utm_source
-order by revenue DESC NULLS LAST;
+select
+  sum(visitors_count),
+  utm_source,
+  sum(total_cost),
+  sum(revenue)
+  from voronka
+  where utm_source = 'vk' or utm_source = 'yandex'
+  group by utm_source
+  order by revenue desc nulls last;
 
-SELECT 
-*,
-total_cost / visitors_count as cpu,
-total_cost / leads_count as cpl,
-total_cost / purchases_count as cppu,
-(revenue - total_cost) / total_cost * 100 as roi
-from voronka;
+SELECT
+  *,
+  total_cost / visitors_count as cpu,
+  total_cost / leads_count as cpl,
+  total_cost / purchases_count as cppu,
+  (revenue - total_cost) / total_cost * 100 as roi
+  from voronka;
+
